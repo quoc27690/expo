90c82562dc35f961c3422a3f61b20881f2c7b1d6
quoclam <quoc27690@gmail.com>
2022-01-26 11:19:58 +0700

    first commit

---
+++ src/app/index.jsx
@@ -0,0 +1,39 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import SplashScreen from "@screens/SplashScreen";
+import HomeStackScreen from "@stack/HomeStack";
+import SettingStackScreen from "@stack/SettingStack";
+import SignInStackScreen from "@stack/SignInStack";
+import React, { useEffect, useState } from "react";
+
+const Tab = createBottomTabNavigator();
+
+function SrcApp() {
+	const [isLoading, setIsloading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsloading(false);
+		}, 3000);
+	}, []);
+
+	if (isLoading) {
+		// We haven't finished checking for the token yet
+		return <SplashScreen />;
+	}
+
+	return (
+		<NavigationContainer>
+			{0 ? (
+				<SignInStackScreen />
+			) : (
+				<Tab.Navigator>
+					<Tab.Screen name="HomeTab" component={HomeStackScreen} />
+					<Tab.Screen name="SettingTab" component={SettingStackScreen} />
+				</Tab.Navigator>
+			)}
+		</NavigationContainer>
+	);
+}
+
+export default SrcApp;
